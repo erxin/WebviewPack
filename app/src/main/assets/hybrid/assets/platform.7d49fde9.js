dfa26a757a50798750944a50c463976133b17032
yixin <2823773469@qq.com>
2024-07-01 14:40:26 +0800

    push

--- app/src/main/assets/hybrid/assets/platform.7d49fde9.js
+++
@@ -1,1 +0,0 @@
-function n(){return!!/JBCWeb/.test(navigator.userAgent)}function t(){var n;return(null==(n=window.getAndroidIDInterface)?void 0:n.getAndroidID())||""}export{t as g,n as i};
--- app/src/main/assets/hybrid/assets/platform.c307c9d4.js
+++
@@ -1,1 +0,0 @@
-function n(){var n;return(null==(n=window.getAndroidIDInterface)?void 0:n.getAndroidID())||""}export{n as g};
