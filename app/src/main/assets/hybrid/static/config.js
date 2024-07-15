484108bd5875f7d1332bace298e2fadf79728759
yixin <2823773469@qq.com>
2024-07-15 15:39:23 +0800

    push

--- app/src/main/assets/hybrid/static/config.js
+++ app/src/main/assets/hybrid/static/config.js
@@ -4,7 +4,7 @@ window.config = () => {
         brand: 'jbc', // 品牌 日本=> jbc, 泰国=>tiger
         webLogo: '', // webLogo有值是优先加载Logo 否则按照地区加载默认图标
         build: 76, // 版本build
-        versionName: '1.0.4.1', // 版本号
+        versionName: '1.0.5', // 版本号
         debug: true, // 是否显示vconsole
         pageSize: 10, // 缓存商品的分页条数设置
     };
